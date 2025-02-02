28fd566c291ec2fa58e95d0791dadf0d81266e5a
VladHolovin27 <vladislavg899@gmail.com>
2025-02-02 11:17:54 +0200

    Updates

--- src/store.jsx
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers'; // Убедись, что у тебя есть reducers/index.js
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
